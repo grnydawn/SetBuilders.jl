35cea07ab448941ec66c78d34b11092e17be64ab
Documenter.jl <documenter@juliadocs.github.io>
2023-11-26 23:30:49 +0000

    build based on fa5d2de

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SetBuilders","category":"page"},{"location":"#SetBuilders","page":"Home","title":"SetBuilders","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SetBuilders.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SetBuilders]","category":"page"}]
+}
