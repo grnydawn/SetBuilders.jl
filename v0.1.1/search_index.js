56492c010436f896b8451ff2eccf5e03a48aad3f
Youngsung Kim <youngsung.kim.act2@gmail.com>
2024-01-17 10:00:38 -0500

    cleans gh-pages

--- v0.1.1/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SetBuilders","category":"page"},{"location":"#SetBuilders","page":"Home","title":"SetBuilders","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SetBuilders.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SetBuilders]","category":"page"}]
-}
